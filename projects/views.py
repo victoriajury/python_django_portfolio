6f8230c7d80fe1cf72965727f997d6dcfc347176
Victoria Jury <victoriaehodges@gmail.com>
2024-10-14 23:04:21 +0200

    update number of image fields, order portfolio items by date

--- projects/views.py
+++ projects/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render
-from projects.models import Project
+from projects.models import IMAGE_COUNT, Project
 
 
 def project_index(request):
-    projects = Project.objects.all()
+    projects = Project.objects.all().order_by("-date")
     context = {"projects": projects}
     return render(request, "projects/project_index.html", context)
 
 
 def project_detail(request, pk):
     project = Project.objects.get(pk=pk)
-    project_images = [project.image1, project.image2, project.image3, project.image4, project.image5]
+    project_images = [project.__getattribute__(f"image{i}") for i in range(1, IMAGE_COUNT + 1)]
     context = {"project": project, "project_images": project_images}
     return render(request, "projects/project_detail.html", context)
