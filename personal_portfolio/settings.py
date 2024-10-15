a5436cc47a11ffe0bbf7038a99b84b92d3ececd7
Victoria Jury <victoriaehodges@gmail.com>
2024-10-15 10:15:49 +0200

    add email obfuscator

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "email_obfuscator",
+
     # local apps
     "pages.apps.PagesConfig",
     "projects.apps.ProjectsConfig",
