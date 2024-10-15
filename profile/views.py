1a7971c93d56b2b9da1d3f807d9590518cd1f980
Victoria Jury <victoriaehodges@gmail.com>
2024-10-15 23:14:34 +0200

    add position column to resume sections

--- profile/views.py
+++ profile/views.py
@@ -5,10 +5,10 @@ from django.shortcuts import render
 
 def home(request):
     resume_summary = ResumeSummary.objects.first()
-    resume_education = ResumeEducation.objects.all()
-    technical_skills = ResumeSkill.objects.filter(category="Technical Skills")
-    languages = ResumeSkill.objects.filter(category="Languages")
-    resume_experience = ResumeExperience.objects.all()
+    resume_education = ResumeEducation.objects.all().order_by("position")
+    technical_skills = ResumeSkill.objects.filter(category="Technical Skills").order_by("position")
+    languages = ResumeSkill.objects.filter(category="Languages").order_by("position")
+    resume_experience = ResumeExperience.objects.all().order_by("position")
     context = {
         "summary": resume_summary,
         "education": resume_education,
