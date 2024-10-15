5a044163ab0ad3219b53355a73ad0f5e7dd3d777
Victoria Jury <victoriaehodges@gmail.com>
2024-10-15 10:17:04 +0200

    linting

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "email_obfuscator",
-
     # local apps
     "pages.apps.PagesConfig",
     "projects.apps.ProjectsConfig",
