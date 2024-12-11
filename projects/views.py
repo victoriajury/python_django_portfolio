964cbabab8ad6d2ac672b22d0fcde1900b2a63a7
Victoria Jury <victoriaehodges@gmail.com>
2024-12-11 20:01:47 +0100

    update page titles

--- projects/views.py
+++ projects/views.py
@@ -1,15 +1,25 @@
 from django.shortcuts import render
 from projects.models import IMAGE_COUNT, Project
+from profile.models import ResumeSummary
 
 
 def project_index(request):
     projects = Project.objects.all().order_by("-date")
-    context = {"projects": projects}
+    resume_summary = ResumeSummary.objects.first()
+    context = {
+        "projects": projects,
+        "summary": resume_summary
+        }
     return render(request, "projects/project_index.html", context)
 
 
 def project_detail(request, pk):
     project = Project.objects.get(pk=pk)
     project_images = [project.__getattribute__(f"image{i}") for i in range(1, IMAGE_COUNT + 1)]
-    context = {"project": project, "project_images": project_images}
+    resume_summary = ResumeSummary.objects.first()
+    context = {
+        "project": project,
+        "project_images": project_images,
+        "summary": resume_summary
+        }
     return render(request, "projects/project_detail.html", context)
