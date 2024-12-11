0e59a913e8a380683b60c3a42ad1ca9ec3773c88
Victoria Jury <victoriaehodges@gmail.com>
2024-12-11 20:03:52 +0100

    fix linting

--- projects/views.py
+++ projects/views.py
@@ -1,15 +1,13 @@
+from profile.models import ResumeSummary
+
 from django.shortcuts import render
 from projects.models import IMAGE_COUNT, Project
-from profile.models import ResumeSummary
 
 
 def project_index(request):
     projects = Project.objects.all().order_by("-date")
     resume_summary = ResumeSummary.objects.first()
-    context = {
-        "projects": projects,
-        "summary": resume_summary
-        }
+    context = {"projects": projects, "summary": resume_summary}
     return render(request, "projects/project_index.html", context)
 
 
@@ -17,9 +15,5 @@ def project_detail(request, pk):
     project = Project.objects.get(pk=pk)
     project_images = [project.__getattribute__(f"image{i}") for i in range(1, IMAGE_COUNT + 1)]
     resume_summary = ResumeSummary.objects.first()
-    context = {
-        "project": project,
-        "project_images": project_images,
-        "summary": resume_summary
-        }
+    context = {"project": project, "project_images": project_images, "summary": resume_summary}
     return render(request, "projects/project_detail.html", context)
